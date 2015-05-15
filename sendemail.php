f347fdcb5973c5e5467ebef858fe34405b82dc47
mjota <manuel.pol@gmail.com>
2015-05-15 21:51:25 +0200

    Edit

---
+++ sendemail.php
@@ -0,0 +1,21 @@
+<?php
+	header('Content-type: application/json');
+	$status = array(
+		'type'=>'success',
+		'message'=>'Gràcies pel missatge! Aviat et contestarem! '
+	);
+
+    $name = @trim(stripslashes($_POST['name'])); 
+    $email = @trim(stripslashes($_POST['email'])); 
+    $message = @trim(stripslashes($_POST['message']));
+    $telefon = @trim(stripslashes($_POST['telefon']));
+
+    $email_from = 'admin@cambiodeidea.com';
+    $email_to = 'manuel.pol@gmail.com';//replace with your email
+
+    $body = 'Nom: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Telefon: ' . $telefon . "\n\n" . 'Missatge: ' . $message;
+
+    $success = @mail($email_to, 'Email de la web', $body, 'From: <'.$email_from.'>');
+
+    echo json_encode($status);
+    die;
